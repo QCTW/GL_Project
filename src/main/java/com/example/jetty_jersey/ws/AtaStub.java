9499eca67af006f66caabaace4d69fa21c58c0f4
mohamessi <mo-2010@live.fr>
2017-02-20 19:13:17 +0100

    bouchon

--- src/main/java/com/example/jetty_jersey/ws/AtaStub.java
+++ src/main/java/com/example/jetty_jersey/ws/AtaStub.java
@@ -1,5 +1,6 @@
 package com.example.jetty_jersey.ws;
 
+import com.example.jetty_jersey.Dao.*;
 import javax.ws.rs.Path;
 
 @Path("/ata")
---
+++ src/main/java/com/example/jetty_jersey/ws/FlightStub.java
@@ -0,0 +1,9 @@
+package com.example.jetty_jersey.ws;
+
+import javax.ws.rs.Path;
+import com.example.jetty_jersey.Dao.*;
+
+@Path("/Flight")
+public class FlightStub {
+
+}
