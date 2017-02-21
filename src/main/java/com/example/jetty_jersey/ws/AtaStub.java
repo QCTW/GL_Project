4baa79b1e79f9bafe7cc719c3673868b2c32e9a0
mohamessi <mo-2010@live.fr>
2017-02-21 15:10:55 +0100

    bouchon

--- src/main/java/com/example/jetty_jersey/ws/AtaStub.java
+++ src/main/java/com/example/jetty_jersey/ws/AtaStub.java
@@ -1,7 +1,11 @@
 package com.example.jetty_jersey.ws;
 
-import com.example.jetty_jersey.Dao.*;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/ata")
 public class AtaStub {
--- src/main/java/com/example/jetty_jersey/ws/LoginStub.java
+++ src/main/java/com/example/jetty_jersey/ws/LoginStub.java
@@ -1,6 +1,10 @@
 package com.example.jetty_jersey.ws;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.example.jetty_jersey.Dao.*;
 
