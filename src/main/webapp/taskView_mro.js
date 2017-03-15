9742006cd703b1534c5f7ce2176fa6e143518657
jude ingani <judeingani@hotmail.com>
2017-03-15 16:00:35 +0100

    js de tastView

---
+++ src/main/webapp/taskView_mro.js
@@ -0,0 +1,75 @@
+
+function getServerData(url, success){
+    $.ajax({
+        dataType: "json",
+        url: url
+    }).done(success);
+}
+var task;
+
+function getTask(taskInfo){
+	var templateTaskView = _.template($('#taskViewScript').html());
+	var html = templateTaskView({
+		"ata":JSON.stringify(taskInfo.plane.planeType),
+		
+
+		});
+	$('#taskView').append(html);
+}
+
+function getAllTasks(result){
+	var tab =$('#example').DataTable( {
+    } );
+	for(var i=0; i<result.length; i++){
+		tab.row.add( [
+			JSON.stringify(result[i].id),
+			new Date(result[i].startTime),
+			new Date(result[i].endTime),
+			JSON.stringify(result[i].description),
+			JSON.stringify(result[i].periodicity),
+			JSON.stringify(result[i].ataCategory),
+			JSON.stringify(result[i].hangarNeed)
+		] ).draw( false );
+		//var templateExample = _.template($('#taskTemp').html());
+		/*var html = templateExample({
+			"starttimetask":JSON.stringify(result[i].startTime),
+			"endtimetask":JSON.stringify(result[i].endTime),
+			"descriptiontask":JSON.stringify(result[i].description),
+			"periodicitytask":JSON.stringify(result[i].periodicity),
+			"atatask":JSON.stringify(result[i].ataCategory),
+			"hangartask":JSON.stringify(result[i].hangarNeed),
+		
+		});
+		$('#tabb').append(html);*/
+	}
+}
+
+
+
+
+
+$(function(){
+		getServerData("ws/task/1",getTask);
+});
+/*
+function callDone(result){
+	var templateExample = _.template($('#templateExample').html());
+
+	var html = templateExample({
+		"attribute":JSON.stringify(result)
+	});
+
+	$("#result").append(html);
+}
+
+$(function(){
+	$("#button").click(function(){
+		getServerData("ws/example/aircraft",callDone);
+	});
+});
+$(function(){
+	$("#button2").click(function(){
+		getServerData("ws/example/aircraft",callDone);
+	});
+});
+*/
