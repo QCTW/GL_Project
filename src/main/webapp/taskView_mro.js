3f1611520dc327d0a5e2310eaba931d12f57a116
mohamessi <mo-2010@live.fr>
2017-03-19 19:27:35 +0100

    cleaning

--- src/main/webapp/taskView_mro.js
+++ src/main/webapp/taskView_mro.js
@@ -17,59 +17,9 @@ function getTask(taskInfo){
 	$('#taskView').append(html);
 }
 
-function getAllTasks(result){
-	var tab =$('#example').DataTable( {
-    } );
-	for(var i=0; i<result.length; i++){
-		tab.row.add( [
-			JSON.stringify(result[i].id),
-			new Date(result[i].startTime),
-			new Date(result[i].endTime),
-			JSON.stringify(result[i].description),
-			JSON.stringify(result[i].periodicity),
-			JSON.stringify(result[i].ataCategory),
-			JSON.stringify(result[i].hangarNeed)
-		] ).draw( false );
-		//var templateExample = _.template($('#taskTemp').html());
-		/*var html = templateExample({
-			"starttimetask":JSON.stringify(result[i].startTime),
-			"endtimetask":JSON.stringify(result[i].endTime),
-			"descriptiontask":JSON.stringify(result[i].description),
-			"periodicitytask":JSON.stringify(result[i].periodicity),
-			"atatask":JSON.stringify(result[i].ataCategory),
-			"hangartask":JSON.stringify(result[i].hangarNeed),
-		
-		});
-		$('#tabb').append(html);*/
-	}
-}
-
-
 
 
 
 $(function(){
 		getServerData("ws/task/1",getTask);
 });
-/*
-function callDone(result){
-	var templateExample = _.template($('#templateExample').html());
-
-	var html = templateExample({
-		"attribute":JSON.stringify(result)
-	});
-
-	$("#result").append(html);
-}
-
-$(function(){
-	$("#button").click(function(){
-		getServerData("ws/example/aircraft",callDone);
-	});
-});
-$(function(){
-	$("#button2").click(function(){
-		getServerData("ws/example/aircraft",callDone);
-	});
-});
-*/
