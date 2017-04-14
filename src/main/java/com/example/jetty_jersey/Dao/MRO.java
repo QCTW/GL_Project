d169c609c14c72981c7c9e9f35cc8fd4ae536074
Quincy Hsieh <quincy.tw@gmail.com>
2017-04-14 12:46:13 +0200

    Remove setId : Id must be set by the constructor and should never be modifiied

--- src/main/java/com/example/jetty_jersey/Dao/MRO.java
+++ src/main/java/com/example/jetty_jersey/Dao/MRO.java
@@ -1,55 +1,53 @@
 package com.example.jetty_jersey.Dao;
 
 /**
- 
- PUT mro/id
- {
- 
- "qualification" : "example of qualification"
- "name" : "example of name"
- "surname" : "example of surname"
- }
+ * 
+ * PUT mro/id
+ * {
+ * 
+ * "qualification" : "example of qualification"
+ * "name" : "example of name"
+ * "surname" : "example of surname"
+ * }
+ * 
+ */
 
-*/
+public class MRO
+{
 
-public class MRO{
-	
-	private String qualification;
+	private String qualification = "N/A";
 	private String name;
-	//private String surname;
-	private int id;
-	
-	public MRO() {
-		id = (int)(Math.random()*100);
-		name= "name"+id;
-		qualification = "qualif"+id;
+	private final int id;
+
+	public MRO(String name, int mroId)
+	{
+		this.name = name;
+		this.id = mroId;
 	}
 
-	public String getQualification() {
+	public String getQualification()
+	{
 		return qualification;
 	}
 
-	public void setQualification(String qualification) {
+	public void setQualification(String qualification)
+	{
 		this.qualification = qualification;
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		this.name = name;
 	}
 
-	public int getId() {
+	public int getId()
+	{
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-
-
-	
 }
