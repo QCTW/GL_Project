8d9a330facc5fd0fb5da8a7644cfef3b48b6c009
FEKIH AHMED meriem <fekihahm@informatique.univ-paris-diderot.fr>
2017-04-17 21:35:52 +0200

    fix changes

---
+++ src/main/java/com/example/jetty_jersey/util/TaskInfo.java
@@ -0,0 +1,24 @@
+package com.example.jetty_jersey.util;
+
+import com.example.jetty_jersey.Dao.Flight;
+import com.example.jetty_jersey.Dao.MRO;
+import com.example.jetty_jersey.Dao.Plane;
+import com.example.jetty_jersey.Dao.Task;
+
+public class TaskInfo
+{
+
+	public Task task;
+	public Plane plane;
+	public Flight flight;
+	public MRO mro; // To check with Mohamad if MRO should be here or in the Task
+
+	public TaskInfo(Task task, Plane plane, Flight flight, MRO mro)
+	{
+		this.task = task;
+		this.plane = plane;
+		this.flight = flight;
+		this.mro = mro;
+	}
+
+}
