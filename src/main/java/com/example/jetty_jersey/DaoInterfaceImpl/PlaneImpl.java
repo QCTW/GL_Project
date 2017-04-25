0f34a048fe73dc4686e932fe4002a4dc95194fd8
Quincy Hsieh <quincy.tw@gmail.com>
2017-04-25 18:15:43 +0200

    Add PlaneStub implementation

--- src/main/java/com/example/jetty_jersey/DaoInterfaceImpl/PlaneImpl.java
+++ src/main/java/com/example/jetty_jersey/DaoInterfaceImpl/PlaneImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.example.jetty_jersey.Dao.Plane;
+import com.example.jetty_jersey.Dao.Status;
 import com.example.jetty_jersey.DaoInterface.PlaneDao;
 import com.example.jetty_jersey.db.DatabaseConnecter;
 import com.example.jetty_jersey.db.Utility;
@@ -26,6 +27,20 @@ public class PlaneImpl implements PlaneDao
 		return pl;
 	}
 
+	public List<Plane> getAllPlanes()
+	{
+		DatabaseConnecter dbc = new DatabaseConnecter();
+		List<Map<String, String>> res = dbc.selectAllFromTableName("plane");
+		dbc.close();
+		List<Plane> pl = new ArrayList<Plane>();
+		for (Map<String, String> m : res)
+		{
+			Plane p = new Plane(Utility.convertIntString(m.get("planeId")), m.get("planetype"));
+			pl.add(p);
+		}
+		return pl;
+	}
+
 	public Plane getPlanebyId(int id)
 	{
 		DatabaseConnecter dbc = new DatabaseConnecter();
@@ -34,6 +49,12 @@ public class PlaneImpl implements PlaneDao
 		Map<String, String> ligne = res.get(0);
 		Plane p = new Plane(Utility.convertIntString(ligne.get("_id")), ligne.get("planetype"));
 		return p;
+	}
+
+	public Status addPlane(Plane p)
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
