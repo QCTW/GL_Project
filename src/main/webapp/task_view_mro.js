f7e8a54b73b9afb906ed26f503f3d1e43c84f4bb
mohamessi <mo-2010@live.fr>
2017-04-29 23:52:17 +0200

    changing task view

--- src/main/webapp/task_view_mro.js
+++ src/main/webapp/task_view_mro.js
@@ -7,76 +7,48 @@ function getServerData(url, success) {
 var task;
 var s;
 function getTask(taskInfo) {
-	task = taskInfo;
-	$('#taskId').html("Task n° "+taskInfo.task.id);
-	$('#ata').html("Ata Category        : "+JSON.stringify(taskInfo.task.ataCategory));
-	$('#startDate').html("Start date    : "+JSON.stringify(taskInfo.task.startTime));
-	$('#endDate').html("End date : "+JSON.stringify(taskInfo.task.endTime));
-	$('#description').html("Description : "+JSON.stringify(taskInfo.task.description));
-	$('#periodicity').html("Periodicity : "+JSON.stringify(taskInfo.task.periodicity));
-	$('#hangarNeed').html("Hangar Need 	: "+taskInfo.task.hangarNeed);
-	$('#mro').html("Mro :  "+taskInfo.mro.name);
-	//});
-	if (taskInfo.tasklist[0].taskStatus == 1) {
-		// s="ws/task/add"
-		$('#buttons')
-				.append(
-						'<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Add</button>');
-		getServerData("ws/mro/all", getAllMro);
-	} else if (taskInfo.tasklist[0].taskStatus == 2) {
-		// s="ws/task/add"
-		$('#buttons')
-				.append(
-						'<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Edit</button>');
-		getServerData("ws/mro/all", getAllMro);
-	} else if (taskInfo.tasklist[0].taskStatus == 3) {
-		// s="ws/task/add"
-		$('#buttons')
-				.append(
-						"<button type=\"button\" class=\"btn btn-default btn-xl\">Remove</button>");
-	}
-	$('#taskView').append(html);
+	$('#taskId').html("Task n° " + taskInfo.task.id);
+	$('#ata').html(JSON.stringify(taskInfo.task.ataCategory));
+	$('#startDate').html(JSON.stringify(taskInfo.task.startTime));
+	$('#endDate').html(JSON.stringify(taskInfo.task.endTime));
+	$('#description').html(JSON.stringify(taskInfo.task.description));
+	$('#periodicity').html(JSON.stringify(taskInfo.task.periodicity));
+	$('#hangarNeed').html(taskInfo.task.hangarNeed);
+	$('#mro').html(taskInfo.mro.name);
 }
 function getAllMro(result) {
-	var cpt=[result.length];
-	var tab = $('#mroList').DataTable({});
+	//var cpt = [ result.length ];
+	var tab = $('#mroList').DataTable({
+		"searching": false
+	});
 	for (var i = 0; i < result.length; i++) {
-		cpt[i]=i;
-		tab.row
-				.add(
-						[
-							
-								JSON.stringify(result[i].id),
-								JSON.stringify(result[i].name),
-								JSON.stringify(result[i].qualification),
-								"<button type='button' class='btn btn-primary' id='button"
-										+ i
-										+ "'>"
-										+ "<span class='glyphicon glyphicon-ok'></span>"
-										+ "</button>"
+		//cpt[i] = i;
+		tab.row.add(
+				[
 
-						]).draw(false);
+				JSON.stringify(result[i].id),
+				JSON.stringify(result[i].name),
+				JSON.stringify(result[i].qualification) ]).draw(false);
 	}
-	$(function(){
-		$("#button"+cpt[i]).click(function(){
-			alert(cpt[i]);
-		});
-	});
 	
-	$('#mroList tbody').on('click', 'tr', function() {
-		if ( $(this).hasClass('selected') ) {
-            $(this).removeClass('selected');
-        }
-        else {
-        	tab.$('tr.selected').removeClass('selected');
-            $(this).addClass('selected');
-        }
-		var data = tab.row(this).data();
-		
-		document.getElementById("mroSelected").innerHTML = "<h5><b>Mro : </b>"+data[1]+"</h5>";
-		
 
-	});
+	$('#mroList tbody')
+			.on(
+					'click',
+					'tr',
+					function() {
+						if ($(this).hasClass('selected')) {
+							$(this).removeClass('selected');
+						} else {
+							tab.$('tr.selected').removeClass('selected');
+							$(this).addClass('selected');
+						}
+						var data = tab.row(this).data();
+
+						document.getElementById("mroSelected").innerHTML = "<h5><b>Mro : </b>"
+								+ data[1] + "</h5>";
+
+					});
 }
 
 var getUrlParameter = function getUrlParameter(sParam) {
@@ -95,4 +67,5 @@ var getUrlParameter = function getUrlParameter(sParam) {
 $(function() {
 	var x = getUrlParameter('id');
 	getServerData("ws/task/" + x, getTask);
+	getServerData("ws/mro/all", getAllMro);
 });
