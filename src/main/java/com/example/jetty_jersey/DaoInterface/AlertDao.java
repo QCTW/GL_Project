85fa75cc316208ba451909bba83484e808669d88
FEKIH AHMED meriem <fekihahm@informatique.univ-paris-diderot.fr>
2017-05-01 12:42:21 +0200

    AlertDao

---
+++ src/main/java/com/example/jetty_jersey/DaoInterface/AlertDao.java
@@ -0,0 +1,38 @@
+package com.example.jetty_jersey.DaoInterface;
+
+import java.util.List;
+
+import com.example.jetty_jersey.Dao.*;
+public interface AlertDao{
+	
+	/**
+	 * Add a new Alert
+	*@param alert
+	**/
+	public void add(Alert alert);
+	/**
+	 * Update an alert
+	*@param alert
+	**/
+	public void update(Alert alert);
+	/**
+	 * Delete an alert by id
+	*@param id
+	**/
+	public void delete(long id);
+	/**
+	 * Get an alert by id
+	*@param id
+	*@return An Alert
+	**/
+	public Alert select(long id);
+	
+	/**
+	 * Get a list of alerts in the following interval (offset, limit)
+	*@param offset
+	*@param limit
+	*@return the alerts list
+	**/
+	public List<Alert> select (int offset,int limit);
+
+}
