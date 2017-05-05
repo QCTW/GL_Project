4311148eba30529fcfee0b75031fae269ac78cff
jude ingani <judeingani@hotmail.com>
2017-05-05 16:40:50 +0200

    functions completed

--- src/main/webapp/tasksMcc.js
+++ src/main/webapp/tasksMcc.js
@@ -53,12 +53,113 @@ function sub(r) {
 	return r.toString().substring(1, r.toString().length - 1);
 }
 
+//<0
 function taskToAssign(){
-	
+	var tr = "";
+	for (var i = 0; i < taskList.length; i++) {
+		if(taskList[i].task.mroId < 0 ){
+			tr += "<tr> "
+			+ "<td>"
+			+ sub(JSON.stringify(result[i].taskGeneric.ataCategory))
+			+ "</td>"
+			+ "<td>"
+			+ sub(JSON.stringify(result[i].task.startTime))
+			+ "</td>"
+			+ "<td>"
+			+ sub(JSON.stringify(result[i].task.endTime))
+			+ "</td>"
+			+ "<td>"
+			+ sub(JSON.stringify(result[i].plane.planeType))
+			+ "</td>"
+			+ "<td>"
+			+ sub(JSON.stringify(result[i].plane.planeId))
+			+ "</td>"
+			+ "<td><button onclick='getSTask("
+			+ sub(JSON.stringify(result[i].task.id))
+			+ ")' "
+			+ "class='btn icon-btn btn-primary'>  "
+			+ "<span class='glyphicon btn-glyphicon glyphicon-eye-open'></span> "
+			+ "</button></td>" + "</tr>";
+		
+		}
+	}
+	$('#tbody').html(tr);
 }
+//>0 and dateNow < Enddate
 function inProgress(){
-	
+	var tr = "";
+	for (var i = 0; i < taskList.length; i++) {
+		if(taskList[i].task.mroId > 0 ){
+			var today = new Date();
+			var endTime = new Date(taskList[i].task.endTime.substring(0,5),taskList[i].task.endTime.substring(6,8),
+					taskList[i].task.endTime.substring(9,11),taskList[i].task.endTime.substring(12,14),
+					taskList[i].task.endTime.substring(15,17));
+			if(today < endTime){
+				tr += "<tr> "
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].taskGeneric.ataCategory))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].task.startTime))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].task.endTime))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].plane.planeType))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].plane.planeId))
+				+ "</td>"
+				+ "<td><button onclick='getSTask("
+				+ sub(JSON.stringify(result[i].task.id))
+				+ ")' "
+				+ "class='btn icon-btn btn-primary'>  "
+				+ "<span class='glyphicon btn-glyphicon glyphicon-eye-open'></span> "
+				+ "</button></td>" + "</tr>";
+			}
+		
+		}
+	}
+	$('#tbody').html(tr);
 }
-function done(){
 	
+
+//>0 and dateNow > EndDate
+function done(){
+	var tr = "";
+	for (var i = 0; i < taskList.length; i++) {
+		if(taskList[i].task.mroId > 0 ){
+			var today = new Date();
+			var endTime = new Date(taskList[i].task.endTime.substring(0,5),taskList[i].task.endTime.substring(6,8),
+					taskList[i].task.endTime.substring(9,11),taskList[i].task.endTime.substring(12,14),
+					taskList[i].task.endTime.substring(15,17));
+			if(today >= endTime){
+				tr += "<tr> "
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].taskGeneric.ataCategory))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].task.startTime))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].task.endTime))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].plane.planeType))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i].plane.planeId))
+				+ "</td>"
+				+ "<td><button onclick='getSTask("
+				+ sub(JSON.stringify(result[i].task.id))
+				+ ")' "
+				+ "class='btn icon-btn btn-primary'>  "
+				+ "<span class='glyphicon btn-glyphicon glyphicon-eye-open'></span> "
+				+ "</button></td>" + "</tr>";
+			}
+		
+		}
+	}
+	$('#tbody').html(tr);
 }
