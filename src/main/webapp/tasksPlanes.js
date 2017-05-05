4c7eea9d9bedb9ee5bc6a35ce01e0759c7eea956
mohamessi <mo-2010@live.fr>
2017-05-05 17:35:12 +0200

    quincy

--- src/main/webapp/tasksPlanes.js
+++ src/main/webapp/tasksPlanes.js
@@ -5,16 +5,15 @@ $(function() {
 	}
 });
 
-
 function uploadMPD() {
 	var file = $("#uploadMPD")[0].files[0];
 	console.log(file);
-	
+
 }
 function uploadFlights() {
 	var file = $("#uploadMPD")[0].files[0];
 	console.log(file);
-	
+
 }
 function getServerData(url, success) {
 	$.ajax({
@@ -72,18 +71,18 @@ function getAllPlanes(result) {
 	var tr = "";
 	for (var i = 0; i < result.length; i++) {
 		tr += "<tr> "
-			+ "<td>"
-			+ sub(JSON.stringify(result[i]['planeId']))
-			+ "</td>"
-			+ "<td>"
-			+ sub(JSON.stringify(result[i]['planeType']))
-			+ "</td>"
-			+ "<td><button onclick='getPlane("
-			+ sub(JSON.stringify(result[i]['planeId']))
-			+ ")' "
-			+ "class='btn icon-btn btn-primary'>  "
-			+ "<span class='glyphicon btn-glyphicon glyphicon-eye-open'></span> "
-			+ "</button></td>" + "</tr>";
+				+ "<td>"
+				+ sub(JSON.stringify(result[i]['planeId']))
+				+ "</td>"
+				+ "<td>"
+				+ sub(JSON.stringify(result[i]['planeType']))
+				+ "</td>"
+				+ "<td><button onclick='getPlane("
+				+ sub(JSON.stringify(result[i]['planeId']))
+				+ ")' "
+				+ "class='btn icon-btn btn-primary'>  "
+				+ "<span class='glyphicon btn-glyphicon glyphicon-eye-open'></span> "
+				+ "</button></td>" + "</tr>";
 	}
 	$('#planeTbody').html(tr);
 	var planes = $('#planeslist').DataTable({});
@@ -109,105 +108,120 @@ function pseudo(result) {
 
 }
 function logout() {
-	localStorage.clear();
+	// localStorage.clear();
+	localStorage.removeItem("mail");
+	localStorage.removeItem("role");
 	getServerData("ws/login/logout", null);
 	document.location.href = "login.html";
 
 }
 
 // CREATE TASK MENU JS
-function showPlanes(result){
+function showPlanes(result) {
 	console.log(result);
 	//
 	var tr = "";
 	for (var i = 0; i < result.length; i++) {
-		tr += "<tr> "
-			+ "<td>"
-			+ sub(JSON.stringify(result[i]['planeId']))
-			+ "</td>"
-			+ "<td>"
-			+ sub(JSON.stringify(result[i]['planeType']))
-			+ "</td>"
-			+ "<td><button onclick='chooseTasksByPlane("
-			+ sub(JSON.stringify(result[i]['planeId']))
-			+ ")' "
-			+ "class='btn icon-btn btn-success'>  "
-			+ "<span class='glyphicon glyphicon-ok'></span> "
-			+ "</button></td>" + "</tr>";
+		var str = sub(JSON.stringify(result[i].planeType)); console.log("str : "+str);
+		tr += "<tr> " 
+				+ "<td>"+ sub(JSON.stringify(result[i]['planeId']))+ "</td>" 
+				+ "<td>"+ sub(JSON.stringify(result[i].planeType)) + "</td>"
+				+ "<td><button onclick='chooseTasksByPlane(Canadair RJ 1000)'"
+				+ "class='btn icon-btn btn-success'>  "
+				+ "<span class='glyphicon glyphicon-ok'></span> "
+				+ "</button></td>" + "</tr>";
 	}
 	$('#planeTbody2').html(tr);
 	$('#planeslist2').DataTable({});
 }
-function choosePlane(){
-	
+function choosePlane() {
+
 }
-function chooseTasksByPlane(id){
-	getServerData("ws/task/all", chooseTasksByPlaneAux);
+var idPlane;
+function chooseTasksByPlane(planetype) {
+	console.log(planetype);
+	idPlane=1;
+	getServerData("ws/task/genericByPlane/"+planetype, chooseTasksByPlaneAux);
 }
 
 var tab;
-function chooseTasksByPlaneAux(result){
+
+function chooseTasksByPlaneAux(result) {
 	var tr = "";
-	var id;
 	// console.log("in get all tasks function and tr = "+tr);
 	for (var i = 0; i < result.length; i++) {
 		tr += "<tr> "
 				+ "<td>"
-				+ sub(JSON.stringify(result[i].taskGeneric.id))
+				+ sub(JSON.stringify(result[i].id))
 				+ "</td>"
 				+ "<td>"
-				+ sub(JSON.stringify(result[i].taskGeneric.ataCategory))
+				+ sub(JSON.stringify(result[i].ataCategory))
 				+ "</td>"
 				+ "<td>"
-				+ sub(JSON.stringify(result[i].plane.planeType))
+				+ sub(JSON.stringify(result[i].planeType))
 				+ "</td>"
 				+ "<td>"
-				+ sub(JSON.stringify(result[i].plane.planeId))
+				+ sub(JSON.stringify(result[i].planeId))
 				+ "</td>"
-				+ "<td><button onclick='callViewTask("+result[i].taskGeneric.id+")' class='btn icon-btn btn-primary' data-toggle='modal' data-target='#myModal'><span class='glyphicon btn-glyphicon glyphicon-eye-open'></span></button></td>"
-				+ "<td><button onclick='getSTask("
-				+ idt
-				+ ")' "
+				+ "<td><button onclick='callViewTask("
+				+ result[i].id
+				+ ")' class='btn icon-btn btn-primary' data-toggle='modal' data-target='#myModal'><span class='glyphicon btn-glyphicon glyphicon-eye-open'></span></button></td>"
+				+ "<td><button onclick='chooseStartDate(" + sub(JSON.stringify(result[i].id)) + ")' "
 				+ "class='btn icon-btn btn-success'>  "
 				+ "<span class='glyphicon glyphicon-ok'></span> "
 				+ "</button></td>" + "</tr>";
 		// console.log(tr);
 	}
+
+	$('#createTaskBody')
+			.html(
+					"<table class='table table-striped'>"
+							+ "<thead><tr><th>task Id</th>  <th >ATA category</th> <th >Plane</th> <th>Plane</th> <th> show tasks </th> <th>Choose Task</th> </tr></thead>"
+							+ "<tbody id='tbody'>" + tr + "</tbody>"
+							+ "</table>");
+}
+var genericTaskId;
+function chooseStartDate(taskId) {
+	genericTaskId = taskId
+	$("#createTaskBody").load("chooseStartDate.html");
 	
-	$('#createTaskBody').html("<table class='table table-striped'>" +
-			"<thead><tr><th>task Id</th>  <th >ATA category</th> <th >Plane</th> <th>Plane</th> <th> show tasks </th> <th>Choose Task</th> </tr></thead>" +
-			"<tbody id='tbody'>"+tr+"</tbody>" +
-			"</table>");
 }
 
-function chooseStartDate(planeId, taskId){
+function validTask() {
+	//var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
 	
+	
+	alert('task id Validate'+'\n plane id = '+planeId+'\n taskId = '+genericTaskId);
+	document.location.href = "tasksPlanes.html";
 }
-function callViewTask(id){
+function callViewTask(id) {
 	getServerData("ws/task/" + id, viewTask)
 }
-function viewTask(task){
+function viewTask(task) {
 	console.log(task);
-	$('#viewTaskGen').html('Task n°'+sub(JSON.stringify(task.taskGeneric.id)));
+	$('#viewTaskGen')
+			.html('Task n°' + sub(JSON.stringify(task.taskGeneric.id)));
 	$('#ata').html(+sub(JSON.stringify(task.taskGeneric.ataCategory)));
 	$('#description').html(sub(JSON.stringify(task.taskGeneric.description)));
 	$('#periodicity').html(sub(JSON.stringify(task.taskGeneric.periodicity)));
-	$('#hangerNeed').html(sub(JSON.stringify((task.taskGeneric.hangarNeed==true)))?'yes':'no');
+	$('#hangerNeed').html(
+			sub(JSON.stringify((task.taskGeneric.hangarNeed == true))) ? 'yes'
+					: 'no');
 	$('#length').html(sub(JSON.stringify(task.taskGeneric.duree)));
 	$('#planeType2').html(sub(JSON.stringify(task.taskGeneric.planeType)));
 	console.log(task.taskGeneric.planeType);
-	
-	//$('#viewTaskGen').html('Task n°'+taskId);
+
+	// $('#viewTaskGen').html('Task n°'+taskId);
 }
 // ----------------------------------------
 
 $(function() {
 	getServerData("ws/task/all", getAllTasks);
-	//getServerData("ws/plane/all",getAllPlanes);
+	// getServerData("ws/plane/all",getAllPlanes);
 	getServerData("ws/plane/all", showPlanes);
 });
 $(function() {
-	
+
 });
 $(function() {
 	$("#pseudo").html(localStorage.getItem("mail"));
