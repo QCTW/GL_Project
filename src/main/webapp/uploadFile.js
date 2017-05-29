048f525d72c8b0e983427092c353ef035f85ab19
bdiabi <binta.dbr@gmail.com>
2017-05-29 15:41:42 +0200

    upload flights

--- src/main/webapp/uploadFile.js
+++ src/main/webapp/uploadFile.js
@@ -12,3 +12,19 @@ function importMDP() {
 	reader.readAsText($("#uploadMPD")[0].files[0]);
 	
 }
+
+
+function uploadFlights() {
+	var file = $("#uploadMPD")[0].files[0];
+	var text;
+	var reader = new FileReader();
+	reader.onload = function() {
+		text = reader.result;
+		// var node = document.getElementById('output');
+		// node.innerText = text;
+		console.log("text " +text);
+		postServerData("ws/Flight/addFlights/"+encodeURIComponent(btoa(text)),alert(text),null);
+	}
+	reader.readAsText($("#uploadFlight")[0].files[0]);
+	
+}
